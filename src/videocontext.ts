5678ecf59d6b52cbea19036103deddde7f63711b
Alan Rogers <alan@dropbox.com>
2023-08-24 14:49:43 +1000

    Undo the webgl change so we can just go straight to using webgl2
    There appeared to be slightly different buffer clearing behaviour.

--- src/videocontext.ts
+++ src/videocontext.ts
@@ -150,7 +150,7 @@ export default class VideoContext {
         this._renderNodeOnDemandOnly = renderNodeOnDemandOnly;
 
         this._gl = canvas.getContext(
-            "webgl",
+            "experimental-webgl",
             Object.assign(
                 { preserveDrawingBuffer: true }, // can be overriden
                 webglContextAttributes,
