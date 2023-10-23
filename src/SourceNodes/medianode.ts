728df65e9b7b14b609f77a121feeacf15eaad92f
Alan Rogers <alan@dropbox.com>
2023-10-23 22:14:56 +1100

    We need to pause and unload when a node moves to the sequenced STATE

--- src/SourceNodes/medianode.ts
+++ src/SourceNodes/medianode.ts
@@ -305,6 +305,10 @@ class MediaNode extends SourceNode {
                 this._unload();
             }
             return false;
+        } else if (this._state === SOURCENODESTATE.sequenced && this._element !== undefined) {
+            this._element.pause();
+            this._unload();
+            return false;
         }
     }
 
